ed7e8f6da0fe7e3a9edb57c9d8c4d5eac58ca6ec
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2020-01-23 16:43:54 -0500

    Creacion primera version de los dados

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,9 @@
 
-let dado = {
-    
+
+function tirarDado(){
+    for(var i=0; i<6; i++){
+        var k = Math.round(Math.random()*5)+1;
+        document.getElementById('dado'+(i+1)).textContent = k;
+        console.log(k)
+    }
 }
